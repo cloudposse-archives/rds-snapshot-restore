45e405ea92221d19ae0537722601cc3f911c16a4
Igor Rodionov <goruha@gmail.com>
2017-06-02 00:08:02 +0600

    Refactored

--- rds_restore_cluster_from_snapshot.sh
+++ rds_restore_cluster_from_snapshot.sh
@@ -8,7 +8,6 @@ SNAPSHOT_ID=$2
 MASTER_PASSWORD=$(MASTER_PASSWORD:-)
 
 REGION=$(REGION:-"us-west-2")
-
 SLEEP=$(SLEEP:-10)
 
 TMP_SUFFIX=$(TMP_SUFFIX:-"-tmp")
@@ -19,324 +18,224 @@ CLUSTER_TO_DELETE="${CLUSTER}${TO_DELETE_SUFFIX}"
 
 DRY_RUN=$(DRY_RUN:-false)
 
-CLUSTER_MAP_QUERY=$( cat <<EOF
-  .DBClusters[0] |
-    {
-      AvailabilityZones: .AvailabilityZones,
-      DBClusterIdentifier: "\(.DBClusterIdentifier)-tmp",
-      SnapshotIdentifier: "${SNAPSHOT_ID}",
-      Engine: .Engine,
-      EngineVersion: .EngineVersion,
-      Port: .Port,
-      DBSubnetGroupName: .DBSubnetGroup,
-      DatabaseName: .DatabaseName,
-      VpcSecurityGroupIds: [.VpcSecurityGroups[].VpcSecurityGroupId],
-      EnableIAMDatabaseAuthentication: .IAMDatabaseAuthenticationEnabled
-    }
-EOF
-)
-
-INSTANCE_MAP_QUERY=$( cat <<EOF
-  .DBInstances[0] |
-    {
-      DBInstanceIdentifier: "\(.DBInstanceIdentifier)${TMP_SUFFIX}",
-      DBInstanceClass: .DBInstanceClass,
-      Engine: .Engine,
-      DBSecurityGroups: .DBSecurityGroups,
-      AvailabilityZone: .AvailabilityZone,
-      DBSubnetGroupName: .DBSubnetGroup.DBSubnetGroupName,
-      PreferredMaintenanceWindow: .PreferredMaintenanceWindow,
-      DBParameterGroupName: .DBParameterGroups[0].DBParameterGroupName,
-      MultiAZ: .MultiAZ,
-      EngineVersion: .EngineVersion,
-      AutoMinorVersionUpgrade: .AutoMinorVersionUpgrade,
-      LicenseModel: .LicenseModel,
-      OptionGroupName: .OptionGroupMemberships[0].OptionGroupName,
-      PubliclyAccessible: .PubliclyAccessible,
-      DBClusterIdentifier: "${CLUSTER_TMP}",
-      StorageType: .StorageType,
-      StorageEncrypted: .StorageEncrypted,
-      CopyTagsToSnapshot: .CopyTagsToSnapshot,
-      MonitoringInterval: .MonitoringInterval,
-      PromotionTier: .PromotionTier,
-    }
-EOF
-)
-
 ####==================== Restoring cluster =================================#####
 
-CLUSTER_JSON=$( aws --region $REGION rds describe-db-clusters --db-cluster-identifier "$CLUSTER" | jq -cM "$CLUSTER_MAP_QUERY" )
-
-if [ "$CLUSTER_JSON" == "" ]
-then
- echo "ERROR: Cluster ${CLUSTER} not exists";
- exit 1;
-fi
-
-TMP_CLUSTER_EXISTS=$( aws --region $REGION rds describe-db-clusters --filter "Name=db-cluster-id,Values=${CLUSTER_TMP}" | \
-                      jq -cr '.DBClusters | length' )
-
-if [ $TMP_CLUSTER_EXISTS -eq 0 ]
-then
-  [ $DRY_RUN == "true" ] || aws --region $REGION rds restore-db-cluster-from-snapshot --cli-input-json "$CLUSTER_JSON"
-  echo "Copying cluster ${CLUSTER} to ${CLUSTER_TMP}......In process"
-else
-  echo "WARNING: Cluster ${CLUSTER_TMP} exists"
-fi
-
-####==================== INSTANCE COUNT INFO =================================#####
-
-INSTANCES_COUNT=$( aws --region $REGION rds describe-db-instances --filter "Name=db-cluster-id,Values=${CLUSTER}" | \
-  jq -cr '.DBInstances | length' \
-)
-echo "Cluster ${CLUSTER} have ${INSTANCES_COUNT} instances"
-
-####==================== Coping instances =================================#####
+copy_cluster "$CLUSTER" "${CLUSTER_TMP}" "${SNAPSHOT_ID}"
 
 readarray -t INSTANCES < <( aws --region $REGION rds describe-db-instances --filter "Name=db-cluster-id,Values=${CLUSTER}" | \
                               jq -cr '.DBInstances[].DBInstanceIdentifier')
 
 for INSTANCE in "${INSTANCES[@]}"; do
-
-  TMP_INSTANCE_EXISTS=$( aws --region $REGION rds describe-db-instances --filter "Name=db-instance-id,Values=${INSTANCE}${TMP_SUFFIX}" | \
-                          jq -cr '.DBInstances | length' )
-
-  if [ $TMP_INSTANCE_EXISTS -eq 0 ]
-  then
-    INSTANCE_JSON=$( aws --region $REGION rds describe-db-instances --db-instance-identifier "$INSTANCE" |
-                  jq -cM "$INSTANCE_MAP_QUERY" )
-
-    [ $DRY_RUN == "true" ] || aws --region $REGION rds create-db-instance --cli-input-json "$INSTANCE_JSON"
-    echo "Copying instance $INSTANCE from ${CLUSTER} to ${CLUSTER_TMP}......In process"
-  else
-      echo "WARNING: Instance ${INSTANCE}${TMP_SUFFIX} exists"
-  fi
+  copy_instance "$INSTANCE" "${INSTANCE}${TMP_SUFFIX}" "${CLUSTER_TMP}"
 done
 
-####==================== Waiting cluster ready =================================#####
+wait_cluster_state "$CLUSTER_TMP" "available"
+wait_all_instances_in_cluster_state "${CLUSTER_TMP}" "available"
 
-while true; do
-  CLUSTER_TMP_STATUS=$(aws --region $REGION rds describe-db-clusters --db-cluster-identifier "$CLUSTER_TMP" | \
-    jq -cr '.DBClusters[0].Status')
+cluster_change_master_password "${CLUSTER_TMP}" "${MASTER_PASSWORD}"
 
-  if [[ $DRY_RUN == "true" ||  "$CLUSTER_TMP_STATUS" == "available" ]]; then
-    echo "Copying cluster ${CLUSTER} to ${CLUSTER_TMP}......Done"
-    break;
-  fi
+rename_cluster "${CLUSTER}" "${CLUSTER_TO_DELETE}"
+rename_cluster "${CLUSTER_TMP}" "${CLUSTER}"
 
-  echo "Copying cluster ${CLUSTER} to ${CLUSTER_TMP}......In process"
-  sleep ${SLEEP}
-done
-
-####==================== Waiting instances ready =================================#####
-
-while true; do
-  COUNT_NOT_READY_INSTANCES=$(aws --region $REGION rds describe-db-instances --filter "Name=db-cluster-id,Values=${CLUSTER_TMP}" | \
-    jq -cr '.DBInstances | map( select( .DBInstanceStatus != "available" )) | length')
-
-  if [[ $DRY_RUN == "true" || "$COUNT_NOT_READY_INSTANCES" == "0" ]]; then
-    echo "Copying of ${INSTANCES_COUNT} instances from cluster ${CLUSTER} to ${CLUSTER_TMP} cluster......Done"
-    break;
-  fi
-
-  echo "$COUNT_NOT_READY_INSTANCES instances in ${CLUSTER_TMP} is not ready yet"
-  sleep ${SLEEP}
-done
-
-####==================== Reset master password =================================#####
-
-echo "Reset master password for cluster ${CLUSTER_TMP}"
-
-[ $DRY_RUN == "true" ] || aws --region $REGION rds modify-db-cluster \
-                              --db-cluster-identifier "${CLUSTER_TMP}" \
-                              --master-user-password "${MASTER_PASSWORD}" \
-                              --apply-immediately
-
-sleep ${SLEEP}
-
-while true; do
-  CLUSTER_TMP_STATUS=$(aws --region $REGION rds describe-db-clusters --db-cluster-identifier "$CLUSTER_TMP" | \
-    jq -cr '.DBClusters[0].Status')
-
-  if [[ $DRY_RUN == "true" ||  "$CLUSTER_TMP_STATUS" == "available" ]]; then
-    echo "Changing master password for cluster ${CLUSTER_TMP}......Done"
-    break;
-  fi
-
-  echo "Changing master password for cluster ${CLUSTER_TMP}......In process"
-  sleep ${SLEEP}
-done
-
-####==================== Rename cluster =================================#####
-
-####==================== Current to delete =================================#####
-
-echo "Rename cluster ${CLUSTER} to ${CLUSTER_TO_DELETE}"
-
-[ $DRY_RUN == "true" ] || aws --region $REGION rds modify-db-cluster \
-                              --db-cluster-identifier "${CLUSTER}" \
-                              --new-db-cluster-identifier "${CLUSTER_TO_DELETE}" \
-                              --apply-immediately
-
-while true; do
-  CLUSTER_STATUS=$(aws --region $REGION rds describe-db-clusters --db-cluster-identifier "$CLUSTER" | \
-    jq -cr '.DBClusters[0].Status')
-
-  if [[ $DRY_RUN == "true" ||  "$CLUSTER_STATUS" == "renaming" ]]; then
-    echo "Rename cluster ${CLUSTER} to ${CLUSTER_TO_DELETE}......In process"
-    break;
-  fi
-
-  echo "Rename cluster ${CLUSTER} to ${CLUSTER_TO_DELETE}......Waiting to start"
-  sleep 1
-done
-
-####==================== Wait Cluster to delete became available =================================#####
-
-while true; do
-  CLUSTER_STATUS=$(aws --region $REGION rds describe-db-clusters --db-cluster-identifier "$CLUSTER_TO_DELETE" | \
-    jq -cr '.DBClusters[0].Status')
-
-  if [[ $DRY_RUN == "true" ||  "$CLUSTER_STATUS" == "available" ]]; then
-    echo "Cluster ${CLUSTER_TO_DELETE}......Available"
-    break;
-  fi
-
-  echo "Cluster ${CLUSTER_TO_DELETE}......not ready"
-  sleep 1
-done
-
-####==================== TMP to Current =================================#####
-
-echo "Rename cluster ${CLUSTER_TMP} to ${CLUSTER}"
-
-[ $DRY_RUN == "true" ] || aws --region $REGION rds modify-db-cluster \
-                              --db-cluster-identifier "${CLUSTER_TMP}" \
-                              --new-db-cluster-identifier "${CLUSTER}" \
-                              --apply-immediately
-
-while true; do
-  CLUSTER_STATUS=$(aws --region $REGION rds describe-db-clusters --db-cluster-identifier "$CLUSTER_TMP" | \
-    jq -cr '.DBClusters[0].Status')
-
-  if [[ $DRY_RUN == "true" ||  "$CLUSTER_STATUS" == "renaming" ]]; then
-    echo "Rename cluster ${CLUSTER_TMP} to ${CLUSTER}......In process"
-    break;
-  fi
-
-  echo "Rename cluster ${CLUSTER_TMP} to ${CLUSTER}......Waiting to start"
-  sleep 1
-done
-
-####==================== Wait Cluster became available =================================#####
-
-while true; do
-  CLUSTER_STATUS=$(aws --region $REGION rds describe-db-clusters --db-cluster-identifier "$CLUSTER" | \
-    jq -cr '.DBClusters[0].Status')
-
-  if [[ $DRY_RUN == "true" ||  "$CLUSTER_STATUS" == "available" ]]; then
-    echo "Cluster ${CLUSTER}......Available"
-    break;
-  fi
-
-  echo "Cluster ${CLUSTER}......not ready"
-  sleep 1
-done
-
-
-####==================== Rename to delete instances =================================#####
+####==================== Rename instances =================================#####
 
 for INSTANCE in "${INSTANCES[@]}"; do
-
-  TMP_INSTANCE_EXISTS=$( aws --region $REGION rds describe-db-instances --filter "Name=db-instance-id,Values=${INSTANCE}" | \
-                          jq -cr '.DBInstances | length' )
-
-  if [ $TMP_INSTANCE_EXISTS -eq 1 ]
-  then
-    [ $DRY_RUN == "true" ] ||  aws --region $REGION rds modify-db-instance \
-                                    --db-instance-identifier "$INSTANCE" \
-                                    --new-db-instance-identifier "${INSTANCE}${TO_DELETE_SUFFIX}" \
-                                    --apply-immediately
-    echo "Rename instance $INSTANCE to ${INSTANCE}${TO_DELETE_SUFFIX}"
-
-    while true; do
-      INSTANCE_STATUS=$(aws --region $REGION rds describe-db-instances --filter "Name=db-instance-id,Values=${INSTANCE}" | \
-      jq -cr '.DBInstances[].DBInstanceStatus')
-
-      if [[ $DRY_RUN == "true" ||  "$INSTANCE_STATUS" == "renaming" ]]; then
-        echo "Rename instance $INSTANCE to ${INSTANCE}${TO_DELETE_SUFFIX}......In process"
-        break;
-      fi
-
-      echo "Rename instance $INSTANCE to ${INSTANCE}${TO_DELETE_SUFFIX}......Waiting to start"
-      sleep 1
-    done
-
-    while true; do
-      INSTANCE_STATUS=$(aws --region $REGION rds describe-db-instances --filter "Name=db-instance-id,Values=${INSTANCE}${TO_DELETE_SUFFIX}" | \
-      jq -cr '.DBInstances[].DBInstanceStatus')
-
-      if [[ $DRY_RUN == "true" ||  "$INSTANCE_STATUS" == "available" ]]; then
-        echo "Instance ${INSTANCE}${TO_DELETE_SUFFIX}......Available"
-        break;
-      fi
-
-      echo "Instance ${INSTANCE}${TO_DELETE_SUFFIX}......Not ready"
-      sleep 1
-    done
-
-  else
-      echo "WARNING: Instance ${INSTANCE} not exists"
-  fi
-
+  rename_instance "${INSTANCE}" "${INSTANCE}${TO_DELETE_SUFFIX}"
 done
 
-####==================== Rename TMP to normal instances =================================#####
-
 for INSTANCE in "${INSTANCES[@]}"; do
-
-  TMP_INSTANCE_EXISTS=$( aws --region $REGION rds describe-db-instances --filter "Name=db-instance-id,Values=${INSTANCE}${TMP_SUFFIX}" | \
-                          jq -cr '.DBInstances | length' )
-
-  if [ $TMP_INSTANCE_EXISTS -eq 1 ]
-  then
-    [ $DRY_RUN == "true" ] ||  aws --region $REGION rds modify-db-instance \
-                                    --db-instance-identifier "${INSTANCE}${TMP_SUFFIX}" \
-                                    --new-db-instance-identifier "${INSTANCE}" \
-                                    --apply-immediately
-    echo "Rename instance ${INSTANCE}${TMP_SUFFIX} to ${INSTANCE}"
-
-    while true; do
-      INSTANCE_STATUS=$(aws --region $REGION rds describe-db-instances --filter "Name=db-instance-id,Values=${INSTANCE}${TMP_SUFFIX}" | \
-      jq -cr '.DBInstances[].DBInstanceStatus')
-
-      if [[ $DRY_RUN == "true" ||  "$INSTANCE_STATUS" == "renaming" ]]; then
-        echo "Rename instance ${INSTANCE}${TMP_SUFFIX} to ${INSTANCE}......In process"
-        break;
-      fi
-
-      echo "Rename instance ${INSTANCE}${TMP_SUFFIX} to ${INSTANCE}......Waiting to start"
-      sleep 1
-    done
-
-    while true; do
-      INSTANCE_STATUS=$(aws --region $REGION rds describe-db-instances --filter "Name=db-instance-id,Values=${INSTANCE}" | \
-      jq -cr '.DBInstances[].DBInstanceStatus')
-
-      if [[ $DRY_RUN == "true" ||  "$INSTANCE_STATUS" == "available" ]]; then
-        echo "Instance ${INSTANCE}......Available"
-        break;
-      fi
-
-      echo "Instance ${INSTANCE}......Not ready"
-      sleep 1
-    done
-
-  else
-      echo "WARNING: Instance ${INSTANCE}${TMP_SUFFIX} not exists"
-  fi
-
-
+  rename_instance "${INSTANCE}${TMP_SUFFIX}" "${INSTANCE}"
 done
 
 echo "......................................................... Done"
+
+
+wait_instance_state() {
+  INSTANCE=$1
+  STATE=$2
+  while true; do
+    INSTANCE_STATUS=$(aws --region $REGION rds describe-db-instances --filter "Name=db-instance-id,Values=${INSTANCE}" | \
+      jq -cr '.DBInstances[].DBInstanceStatus')
+
+    if [[ $DRY_RUN == "true" ||  "$INSTANCE_STATUS" == "${STATE}" ]]; then
+      break;
+    fi
+    echo "Instance ${INSTANCE} is ${INSTANCE_STATUS}"
+    sleep 1
+  done
+}
+
+rename_instance() {
+  INSTANCE_NAME=$1
+  NEW_INSTANCE_NAME=$2
+
+  if [ ! $(instance_exists "$INSTANCE_NAME") ]
+  then
+    [ $DRY_RUN == "true" ] ||  aws --region $REGION rds modify-db-instance \
+                                    --db-instance-identifier "${INSTANCE_NAME}" \
+                                    --new-db-instance-identifier "${NEW_INSTANCE_NAME}" \
+                                    --apply-immediately
+    echo "Rename instance ${INSTANCE_NAME} to ${NEW_INSTANCE_NAME}"
+
+    wait_instance_state "${INSTANCE_NAME}" "renaming"
+    wait_instance_state "${NEW_INSTANCE_NAME}" "available"
+  else
+      echo "WARNING: Instance ${INSTANCE_NAME} not exists"
+  fi
+}
+
+wait_cluster_state() {
+  CLUSTER=$1
+  STATE=$2
+  while true; do
+    CLUSTER_STATUS=$(aws --region $REGION rds describe-db-clusters --db-cluster-identifier "$CLUSTER" | \
+      jq -cr '.DBClusters[0].Status')
+
+    if [[ $DRY_RUN == "true" ||  "$CLUSTER_STATUS" == "${STATE}" ]]; then
+      break;
+    fi
+
+    echo "Cluster ${CLUSTER} is ${CLUSTER_STATUS}"
+    sleep 1
+  done
+}
+
+wait_all_instances_in_cluster_state() {
+  local CLUSTER=$1
+  local STATE=$2
+  while true; do
+    COUNT_NOT_READY_INSTANCES=$(aws --region $REGION rds describe-db-instances --filter "Name=db-cluster-id,Values=${CLUSTER}" | \
+      jq -cr '.DBInstances | map( select( .DBInstanceStatus != "${STATE}" )) | length')
+
+    if [[ $DRY_RUN == "true" || "$COUNT_NOT_READY_INSTANCES" == "0" ]]; then
+      echo "All instances in cluster ${CLUSTER} are ${STATE}"
+      break;
+    fi
+
+    echo "$COUNT_NOT_READY_INSTANCES instances in ${CLUSTER} is not ${STATE}"
+    sleep ${SLEEP}
+  done
+}
+
+
+rename_cluster() {
+  local CLUSTER_NAME=$1
+  local NEW_CLUSTER_NAME=$2
+
+  echo "Rename cluster ${CLUSTER_NAME} to ${NEW_CLUSTER_NAME}"
+
+  [ $DRY_RUN == "true" ] || aws --region $REGION rds modify-db-cluster \
+                                --db-cluster-identifier "${CLUSTER_NAME}" \
+                                --new-db-cluster-identifier "${NEW_CLUSTER_NAME}" \
+                                --apply-immediately
+
+  wait_cluster_state "$CLUSTER_NAME" "renaming"
+  wait_cluster_state "$NEW_CLUSTER_NAME" "available"
+}
+
+cluster_change_master_password() {
+  local CLUSTER=$1
+  local PASSWORD=$2
+
+  [ $DRY_RUN == "true" ] || aws --region $REGION rds modify-db-cluster \
+                                --db-cluster-identifier "${CLUSTER}" \
+                                --master-user-password "${PASSWORD}" \
+                                --apply-immediately
+  echo "Reset master password for cluster ${CLUSTER}"
+  sleep ${SLEEP}
+  wait_cluster_state "$CLUSTER" "available"
+}
+
+copy_cluster() {
+  local SOURCE=$1
+  local TARGET=$2
+  local SNAPSHOT=$3
+
+  local CLUSTER_MAP_QUERY=$( cat <<EOF
+    .DBClusters[0] |
+      {
+        AvailabilityZones: .AvailabilityZones,
+        DBClusterIdentifier: "\(.DBClusterIdentifier)-tmp",
+        SnapshotIdentifier: "${SNAPSHOT}",
+        Engine: .Engine,
+        EngineVersion: .EngineVersion,
+        Port: .Port,
+        DBSubnetGroupName: .DBSubnetGroup,
+        DatabaseName: .DatabaseName,
+        VpcSecurityGroupIds: [.VpcSecurityGroups[].VpcSecurityGroupId],
+        EnableIAMDatabaseAuthentication: .IAMDatabaseAuthenticationEnabled
+      }
+EOF
+  )
+
+  CLUSTER_JSON=$( aws --region $REGION rds describe-db-clusters --db-cluster-identifier "$SOURCE" | jq -cM "$CLUSTER_MAP_QUERY" )
+
+  if [ "$CLUSTER_JSON" == "" ]
+  then
+   echo "ERROR: Cluster ${SOURCE} not exists";
+   exit 1;
+  fi
+
+  TMP_CLUSTER_EXISTS=$( aws --region $REGION rds describe-db-clusters --filter "Name=db-cluster-id,Values=${TARGET}" | \
+                        jq -cr '.DBClusters | length' )
+
+  if [ $TMP_CLUSTER_EXISTS -eq 0 ]
+  then
+    [ $DRY_RUN == "true" ] || aws --region $REGION rds restore-db-cluster-from-snapshot --cli-input-json "$CLUSTER_JSON"
+    echo "Copying cluster ${SOURCE} to ${TARGET}......In process"
+  else
+    echo "WARNING: Cluster ${TARGET} exists"
+  fi
+}
+
+copy_instance() {
+  local SOURCE=$1
+  local TARGET=$2
+  local CLUSTER=$3
+
+  local INSTANCE_MAP_QUERY=$( cat <<EOF
+    .DBInstances[0] |
+      {
+        DBInstanceIdentifier: "${TARGET}",
+        DBInstanceClass: .DBInstanceClass,
+        Engine: .Engine,
+        DBSecurityGroups: .DBSecurityGroups,
+        AvailabilityZone: .AvailabilityZone,
+        DBSubnetGroupName: .DBSubnetGroup.DBSubnetGroupName,
+        PreferredMaintenanceWindow: .PreferredMaintenanceWindow,
+        DBParameterGroupName: .DBParameterGroups[0].DBParameterGroupName,
+        MultiAZ: .MultiAZ,
+        EngineVersion: .EngineVersion,
+        AutoMinorVersionUpgrade: .AutoMinorVersionUpgrade,
+        LicenseModel: .LicenseModel,
+        OptionGroupName: .OptionGroupMemberships[0].OptionGroupName,
+        PubliclyAccessible: .PubliclyAccessible,
+        DBClusterIdentifier: "${CLUSTER}",
+        StorageType: .StorageType,
+        StorageEncrypted: .StorageEncrypted,
+        CopyTagsToSnapshot: .CopyTagsToSnapshot,
+        MonitoringInterval: .MonitoringInterval,
+        PromotionTier: .PromotionTier,
+      }
+EOF
+  )
+
+  if [ $(instance_exists "$TARGET") ]
+  then
+    INSTANCE_JSON=$( aws --region $REGION rds describe-db-instances --db-instance-identifier "$SOURCE" |
+                  jq -cM "$INSTANCE_MAP_QUERY" )
+
+    [ $DRY_RUN == "true" ] || aws --region $REGION rds create-db-instance --cli-input-json "$INSTANCE_JSON"
+    echo "Copying instance $SOURCE to ${TARGET} ......In process"
+  else
+    echo "WARNING: Instance ${TARGET} exists"
+  fi
+}
+
+instance_exists() {
+  local INSTANCE=$1
+
+  TMP_INSTANCE_EXISTS=$( aws --region $REGION rds describe-db-instances --filter "Name=db-instance-id,Values=${INSTANCE}" | \
+                          jq -cr '.DBInstances | length' )
+  echo [ $TMP_INSTANCE_EXISTS -neq 0 ]
+}
